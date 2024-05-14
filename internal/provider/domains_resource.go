d477407d29372ffb6232dc8eec6d7c4bd4862915
jon r <jon@allmende.io>
2024-05-14 05:00:58 +0200

    chore(wip)

--- internal/provider/domains_resource.go
+++ internal/provider/domains_resource.go
@@ -3,97 +3,97 @@ package provider
 import (
 	"context"
 
+	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-var _ resource.Resource = (*domainResource)(nil)
+var (
+	_ resource.Resource = &domainsResource{}
+)
 
-func NewDomainResource() resource.Resource {
-	return &domainResource{}
+func NewDomainsResource() resource.Resource {
+	return &domainsResource{}
 }
 
-type domainResource struct{}
+type domainsResource struct{
 
-type domainResourceModel struct {
+}
+
+type domainsResourceModel struct {
 	Id types.String `tfsdk:"id"`
 }
 
-func (r *domainResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
-	resp.TypeName = req.ProviderTypeName + "_domain"
+func (r *domainsResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_domains"
 }
 
-func (r *domainResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+func (r *domainsResource) Schema(_ context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
 	resp.Schema = schema.Schema{
-		Attributes: map[string]schema.Attribute{
-			"id": schema.StringAttribute{
-				Computed: true,
-			},
-		},
+
 	}
 }
 
-func (r *domainResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-	var data domainResourceModel
+func (r *domainsResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var data domainsResourceModel
 
-	// Read Terraform plan data into the model
 	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
-
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// Create API call logic
+	resp.Diagnostics.Append(callDomainsAPI(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
 
-	// Example data value setting
-	data.Id = types.StringValue("example-id")
-
-	// Save data into Terraform state
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
-func (r *domainResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
-	var data domainResourceModel
+func (r *domainsResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var data domainsResourceModel
 
-	// Read Terraform prior state data into the model
 	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
-
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// Read API call logic
-
-	// Save updated data into Terraform state
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
-func (r *domainResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-	var data domainResourceModel
+func (r *domainsResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var data domainsResourceModel
 
-	// Read Terraform plan data into the model
 	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
-
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// Update API call logic
+	resp.Diagnostics.Append(callDomainsAPI(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
 
-	// Save updated data into Terraform state
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
-func (r *domainResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
-	var data domainResourceModel
+func (r *domainsResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var data domainsResourceModel
 
-	// Read Terraform prior state data into the model
 	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
-
 	if resp.Diagnostics.HasError() {
 		return
 	}
+}
 
-	// Delete API call logic
+// Typically this method would contain logic that makes an HTTP call to a remote API, and then stores
+// computed results back to the data model. For example purposes, this function just sets all unknown
+// domains values to null to avoid data consistency errors.
+func callDomainsAPI(ctx context.Context, domains *domainsResourceModel) diag.Diagnostics {
+	if domains.Id.IsUnknown() {
+		domains.Id = types.String{}
+	}
+
+	return nil
 }
